0c4a3a1311cf6b73ba57058837babda131ff26bd
Neeraj Sewani <neerajsewanisrt8@gmail.com>
2019-01-23 18:10:16 +0530

    ImageUpload activity added.

---
+++ app/src/main/java/com/example/neerajsewani/messengerappkotlin/ImageUpload.kt
@@ -0,0 +1,116 @@
+package com.example.neerajsewani.messengerappkotlin
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.squareup.picasso.Picasso
+import id.zelory.compressor.Compressor
+import kotlinx.android.synthetic.main.image_upload.*
+import okhttp3.internal.io.FileSystem
+import java.io.File
+import java.net.URI
+import java.util.*
+import android.provider.MediaStore.Images
+import android.graphics.Bitmap
+import java.io.ByteArrayOutputStream
+
+
+class ImageUpload : AppCompatActivity() {
+    private val IMAGE_PICK: Int = 100
+
+    lateinit var profileImageRef: StorageReference
+    lateinit var usersReference: FirebaseFirestore
+    lateinit var userId: String
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.image_upload)
+        title = "Upload Image"
+
+        usersReference = FirebaseFirestore.getInstance()
+
+        userId = intent.getStringExtra("userId")
+
+        //  onClick
+        circle_image_view_image_upload.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            intent.type = "image/*"
+            startActivityForResult(intent, IMAGE_PICK)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        //  for image capture
+        if (requestCode == IMAGE_PICK && resultCode == Activity.RESULT_OK && data != null) {
+            //  loading the image
+            val imageUri = data.data
+
+            //  generating  random id
+            val filename = UUID.randomUUID().toString()
+
+            profileImageRef = FirebaseStorage.getInstance().getReference("images/$filename")
+
+            //  uploading image to the Storage
+            profileImageRef.putFile(imageUri!!)
+                .addOnSuccessListener {
+
+                    //  getting the download Uri
+                    profileImageRef.downloadUrl.addOnSuccessListener {
+                        Log.d("SignUpScreen", "onActivityResult (line 77): image URL ==> $it")
+
+                        val imageURL = it.toString()
+
+
+                        //  loading the image in the image view
+                        Picasso.get()
+                            .load(imageURL)
+                            .error(R.drawable.ic_person_black_24dp)
+                            .placeholder(R.drawable.ic_person_black_24dp)
+                            .into(circle_image_view_image_upload)
+
+                        //  uploading it to the firestore
+                        usersReference.collection("users").whereEqualTo("userId", userId)
+                            .get()
+                            .addOnSuccessListener {
+                                if (!it.isEmpty) {
+                                    it.forEach {
+                                        //  uploading user's image
+                                        usersReference.collection("users").document(it.id)
+                                            .update("imageURL", imageURL)
+                                            .addOnSuccessListener {
+                                                intent = Intent(this, ChatActivity::class.java)
+                                                intent.flags =
+                                                        Intent.FLAG_ACTIVITY_NEW_TASK.or(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                                                startActivity(intent)
+                                            }.addOnFailureListener {
+                                                Log.e("ImageUpload", "onActivityResult (line 74): ", it)
+                                            }
+                                    }
+
+                                }
+                            }.addOnFailureListener {
+                                Log.e("ImageUpload", "onActivityResult (line 67): ", it)
+                            }
+                    }
+                        .addOnFailureListener {
+                            Log.e("SignUpScreen", "onActivityResult (line 85): $it")
+                        }
+
+                }
+                .addOnFailureListener {
+                    Log.e("SignUpScreen", "onActivityResult (line 87): $it")
+                }
+
+        }
+    }
+}
--- app/src/main/java/com/example/neerajsewani/messengerappkotlin/SignUpScreen.kt
+++ app/src/main/java/com/example/neerajsewani/messengerappkotlin/SignUpScreen.kt
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
-import com.example.neerajsewani.messengerappkotlin.data_class.Users
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
@@ -14,14 +13,13 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.signup_screen.*
 import java.util.*
 import kotlin.collections.HashMap
 
 class SignUpScreen : AppCompatActivity() {
 
     lateinit var firebaseAuth: FirebaseAuth
-    lateinit var profileImageRef: StorageReference
     lateinit var database: FirebaseFirestore
 
     lateinit var email: String
@@ -30,11 +28,9 @@ class SignUpScreen : AppCompatActivity() {
     lateinit var imageURL: String
     lateinit var toast: Toast
 
-    var IMAGE_PICK: Int = 100
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.signup_screen)
 
         title = "Sign-up Screen"
 
@@ -45,78 +41,26 @@ class SignUpScreen : AppCompatActivity() {
 
         //  onClick
         signup_button_signup_activity.setOnClickListener {
-//            progress_bar.show() //  displaying the progress bar
 
             username = username_signup_activity.text.toString()
             email = email_signup_activity.text.toString()
             password = password_signup_activity.text.toString()
 
-            if (username.isNotBlank() && email.isNotBlank() && password.isNotBlank() && imageURL.isNotBlank()) {
+            if (username.isNotBlank() && email.isNotBlank() && password.isNotBlank()) {
                 createUser(email, password) //  creating the user
             }
         }
 
-        //  getting the image from the gallery
-        profile_image_signup_activity.setOnClickListener {
-            progress_bar.show() //  displaying the progress bar
 
-            var intent = Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-            intent.type = "image/*"
-            startActivityForResult(intent, IMAGE_PICK)
-        }
 
         //  INTENT "SigninActivity"
         login_instead_signup_activity.setOnClickListener {
-            var intent = Intent(this, SigninActivity::class.java)
+            val intent = Intent(this, SigninActivity::class.java)
             startActivity(intent)
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        //  for image capture
-        if (requestCode == IMAGE_PICK && resultCode == Activity.RESULT_OK && data != null) {
-            //  loading the image
-            var imageUri = data.data
-
-            //  generating  random id
-            var filename = UUID.randomUUID().toString()
-
-            profileImageRef = FirebaseStorage.getInstance().getReference("images/$filename")
-
-            //  uploading image to the Storage
-            profileImageRef.putFile(imageUri!!)
-                .addOnSuccessListener {
-
-                    //  getting the download Uri
-                    profileImageRef.downloadUrl.addOnSuccessListener {
-                        Log.d("SignUpScreen", "onActivityResult (line 77): image URL ==> $it")
-
-                        progress_bar.hide() //  hiding the progress bar
-
-                        imageURL = it.toString()
-
-                        //  loading the image in the image view
-                        Picasso.get()
-                            .load(imageUri)
-                            .error(R.drawable.abc_action_bar_item_background_material)
-                            .placeholder(R.drawable.abc_ic_arrow_drop_right_black_24dp)
-                            .into(profile_image_signup_activity)
-                    }
-                        .addOnFailureListener {
-                            Log.e("SignUpScreen", "onActivityResult (line 85): $it")
-                        }
-
-                }
-                .addOnFailureListener {
-                    Log.e("SignUpScreen", "onActivityResult (line 87): $it")
-                }
-
-        }
-    }
-
-    fun checkForNewUser(){
+    private fun checkForNewUser(){
         if (firebaseAuth.currentUser != null) {
             intent = Intent(this, ChatActivity::class.java)
             startActivity(intent)
@@ -132,22 +76,20 @@ class SignUpScreen : AppCompatActivity() {
                         Toast.LENGTH_SHORT
                     ).show()
 
-                uploadData(firebaseAuth.uid.toString(), username, email, password, imageURL) //  uploading the data of the user
+                uploadData(firebaseAuth.uid.toString(), username, email, password) //  uploading the data of the user
             }
     }
 
 
-    private fun uploadData(userId: String, username: String, email: String, password: String, imageURL: String) {
+    private fun uploadData(userId: String, username: String, email: String, password: String) {
         //  getting an instance of the db
         database = FirebaseFirestore.getInstance()
 
-        var userDetails = HashMap<String, Any>()
+        val userDetails = HashMap<String, Any>()
         userDetails["userId"] = userId
         userDetails["username"] = username
         userDetails["email"] = email
         userDetails["password"] = password
-        userDetails["imageURL"] = imageURL
-
 
         //  adding a new document
         database.collection("users")
@@ -155,11 +97,9 @@ class SignUpScreen : AppCompatActivity() {
             .addOnSuccessListener {
                 Log.d("SignUpScreen", "uploadData (line 154): Data updation successful")
 
-                progress_bar.hide() //  hiding the progress bar
-
-                intent = Intent(this, ChatActivity::class.java)
+                intent = Intent(this, ImageUpload::class.java)
+                intent.putExtra("userId", userId)
                 startActivity(intent)
-                finish()
             }
             .addOnFailureListener {
                 Log.e("SignUpScreen", "uploadData (line 157): $it")
